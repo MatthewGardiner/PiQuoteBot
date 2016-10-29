16c5e9c2791d8b586bc8e0cdbe0a257e0675dc2a
aphe-lion <tom.0505@hotmail.co.uk>
2016-10-30 00:34:13 +0100

    basic rate limiting i am drunk af right now don't question this

--- main.py
+++ main.py
@@ -3,15 +3,31 @@ import os
 import sqlite3
 import random
 import re
+import time
+import threading
+QUEUE_DELAY = 1
+messageQ = {}
 
 def seven(bot, update):
     if re.match("^.*(7|(S|s)even(!*)).*$", update.message.text):
-        update.message.reply_text("Seven!")
+        add_message_to_queue(update.message, "Seven!")
 
 def random_quote(bot, update):
     if len(quote_cache) > 0:
-        update.message.reply_text(random.choice(quote_cache))
+        add_message_to_queue(update.message, random.choice(quote_cache))
 
+def message_queuer():
+    while True:
+        if len(messageQ) > 0:
+            random_idx = random.randint(0, len(messageQ) - 1)
+            msg_parent = messageQ.keys()[random_idx]
+            msg_content = messageQ[msg_parent]
+            msg_parent.reply_text(msg_content)
+            time.sleep(QUEUE_DELAY)
+
+def add_message_to_queue(msg_parent, msg_content):
+    messageQ[msg_parent] = msg_content
+ 
 def create_schema():
     c = conn.cursor()
     c.execute('CREATE TABLE IF NOT EXISTS quotes (text)')
@@ -32,19 +48,20 @@ def quote(bot, update):
     try:
         quote = ' '.join(update.message.text.split(' ')[1:])
     except IndexError:
-        update.message.reply_text('No quote specified!')
+        add_to_message_queue(update.message, 'No quote specified!')
         return
     if quote in quote_cache:
-    	update.message.reply_text('Fuck off, that quote is already added')
+    	add_to_message_queue(update.message, 'Fuck off, that quote is already added')
 	return
     save_to_quotedb(quote)
     quote_cache.append(quote)
-    update.message.reply_text("Quote added!")
+    add_to_message_queue(update.message, "Quote added!")
 
 if __name__ == "__main__":
     quote_cache = []
     conn = sqlite3.connect('quotes.db', check_same_thread=False)
-
+    msgThread = threading.Thread(target=message_queuer)#
+    msgThread.start()
     create_schema()
     load_quotedb()
     updater = Updater(os.environ['TELEGRAM_API_KEY'])
